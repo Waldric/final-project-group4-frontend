f2bcc2433d6c5df253020a6d57883bc0e9860d2e
Kenth Razen Magbanua <you@example.com>
2025-11-16 13:50:24 +0800

    KRMF1.6 - Added the admin dashboard

--- src/pages/DashboardIndex.jsx
+++ src/pages/DashboardIndex.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import { useAuth } from "../contexts/AuthContext";
+import AdminDashboard from "./admin/AdminDashboard";
 import TeacherDashboard from "./teacher/TeacherDashboard";
 
 const DashboardIndex = () => {
@@ -25,26 +26,7 @@ const DashboardIndex = () => {
 
   // Admin Default Page
   if (user.user_type === "Admin") {
-    return (
-      <div className="flex-1 p-4 md:p-8">
-        {/* Page Header */}
-        <Header
-          location={"Dashboard"}
-          subheader={headerContents.headerSubtext}
-        />
-
-        {/* Dashboard Content */}
-        <div className="bg-white rounded-lg shadow-sm border border-gray-200 p-4 md:p-6 min-h-96">
-          <p className="text-gray-500 text-center py-12">
-            Dashboard content for the{" "}
-            <span className="font-semibold text-[#5603AD]">
-              {user.user_type} Role
-            </span>{" "}
-            goes here...
-          </p>
-        </div>
-      </div>
-    );
+    return <AdminDashboard />
   }
 
   // Teacher Default Page
